d93b38551b8dc847cb79b8cb2f00f416d48066c7
Alex Cherniakov <rustavellyne@gmail.com>
2025-10-29 19:58:44 +0200

    finish with no end

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,13 +1,24 @@
 <template>
-  <input type="text" />
-  <p>{{ VICTORY_MESSAGE }}</p>
-  <p>{{ DEFEAT_MESSAGE }}</p>
+  <input
+    v-model="guessInProgress"
+    @keydown.enter="guessSubmitted = guessInProgress"
+    type="text"
+  />
+  <template v-if="guessSubmitted.length > 0 ">
+    <p v-if="guessSubmitted === wordOfTheDay"> {{ VICTORY_MESSAGE }} </p>
+    <p v-else>{{ DEFEAT_MESSAGE }}</p>
+  </template>
 </template>
 
 <script setup lang="ts">
 import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '@/settings';
+import { ref } from 'vue';
 
 defineProps<{
   wordOfTheDay: string
 }>()
+
+const guessInProgress: Ref<string> = ref('');
+const guessSubmitted: Ref<string> = ref('');
+
 </script>
--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,29 +1,38 @@
 import { mount } from '@vue/test-utils'
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/settings';
 import WordleBoard from '../WordleBoard.vue'
+import {beforeEach} from 'vitest';
 
 describe('HelloWorld', () => {
+  let wordOfTheDay: string = 'TESTS';
+  let wrapper: ReturnType<typeof mount>;
+
+  async function playerSubmitGuess(guess: string) {
+    const guessInput = wrapper.find('input[type=text]');
+    await guessInput.setValue(guess);
+    await guessInput.trigger('keydown.enter');
+  }
+
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay } });
+  });
   test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
     // Arrange
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } });
     // Act
-    const guessInput = wrapper.find('input[type=text]');
-    await guessInput.setValue('TESTS');
-    await guessInput.trigger('keydown.enter');
+    await playerSubmitGuess(wordOfTheDay)
 
    // Assert
     expect(wrapper.text()).toContain(VICTORY_MESSAGE);
   });
   test('a defeat message appears if the user makes a guess that is incorrect', async () => {
-    // Arrange
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } });
     // Act
-    const guessInput = wrapper.find('input[type=text]');
-    await guessInput.setValue('WRONG');
-    await guessInput.trigger('keydown.enter');
+    await playerSubmitGuess('WRONG')
 
     // Assert
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE);
   });
-  test.todo('no end-of-game appears if the user has not yet made a guess');
+  test('no end-of-game appears if the user has not yet made a guess', async () => {
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE);
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE);
+  });
 })
